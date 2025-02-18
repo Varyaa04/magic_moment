5c6da45d688cc53512bbed0e36ab239f130c23dd
Varvara Fedorova <fedorova04_varya@mail.ru>
2025-02-18 22:19:34 +0300

    start

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+
 
 void main() {
-  runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Magic Moment',
-              style: TextStyle(
-                fontSize: 34,
-                fontWeight: FontWeight.w100,
-                fontFamily: 'irishGrover',
-                color: Colors.white
-              ),
-              textAlign: TextAlign.center,
-            ),
-            backgroundColor: const Color(0xbf310b46),
-          ),
-          body:  Container(
+  runApp(const startPage());
+}
+
+class startPage extends StatelessWidget {
+  const startPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 246, 222, 255),
+        body:  Center(
+            child: Row(
+              children: [
+                Container(
+                  alignment: Alignment.bottomCenter,
             decoration: BoxDecoration(
-            gradient: LinearGradient(
-            colors: [
-              Color(0xbf310b46),
-              Color(0xbf691fc1),
-              Color(0xbf310b46),
-            ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-            ),
-            // IconButton(
-            //   onPressed: (){} ,
-            //   icon: const Icon(Icons.image),
-            // ),
-          ),
+                      color: Color.fromARGB(255,235,183,183),
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(20)
+                      )
+                  ),
+                  margin: const EdgeInsets.all(10),
+                  height: 100,
+                  width: 100,
+                  child: IconButton(
+                    iconSize: 50,
+                    icon: const Icon(FluentIcons.image_24_regular),
+                    color: Color.fromARGB(255,30,30,30),
+                    onPressed:(){
+                    },
+                  ),
+                ),
+                Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255,235,183,183),
+                        borderRadius: BorderRadius.all(
+                            Radius.circular(20)
+                        )
+                    ),
+                    margin: const EdgeInsets.only(left: 200),
+                    height: 100,
+                    width: 100,
+                    child:IconButton(
+                      iconSize: 50,
+                      icon: const Icon(FluentIcons.layout_column_two_split_left_24_regular),
+                      color: Color.fromARGB(255,30,30,30),
+                      onPressed:(){
+
+                      },
+                    )
+                )
+              ],
+            )
         ),
       ),
-  );
+    );
+  }
 }
